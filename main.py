2c7254e6400d9acbac4a3fee7e84404bea8484a7
GrubHubGitTub <aidangruber@hotmail.com>
2022-02-21 21:34:12 +0700

    update for Selenium 4.0, and add Edge driver

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import requests
-import selenium as s
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver
 from time import sleep
 import os
 import subprocess
 import sys
-# delete this or change the path to load your own .env file (pip install python-dotenv)
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+#   -----------CHOOSE WEBDRIVER (REMOVE HEADLESS OPTION TO SEE SELENIUM RUNNING)------------
+install("webdriver-manager")
+
+# # CHROME DRIVER USE THIS:
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.options import Options
+# options = Options()
+# options.headless = True
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+# # EDGE DRIVER USES THIS:
+# from selenium.webdriver.edge.service import Service
+# from webdriver_manager.microsoft import EdgeChromiumDriverManager
+# from selenium.webdriver.edge.options import Options
+# options = Options()
+# options.add_argument("headless")
+# driver = webdriver.Edge(service = Service(EdgeChromiumDriverManager().install()), options=options)
+
+#-------change env path and variables------------
+install("python-dotenv")
 from dotenv import load_dotenv
-load_dotenv("C:/New/.env")
-#-------------------------------------
+load_dotenv(".env")
 
 # enter your Netflix email/phone number
 email = os.getenv("email")
@@ -22,20 +44,10 @@ user_profile = os.getenv("user_profile")
 # enter your rapid api key (it's free)
 api_key = os.getenv("api_key")
 # Change your country code (see readme for list of countries)
-country = "425"
+country = os.getenv("country")
 
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-install("webdriver-manager")
-from webdriver_manager.chrome import ChromeDriverManager
 
 def get_my_list():
-    # selenium setup- change path to your webdriver
-    op = s.webdriver.ChromeOptions()
-    op.add_argument('headless')
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=op)
 
     # go to netflix
     print("Going to Netflix...")
@@ -44,27 +56,28 @@ def get_my_list():
     # click login button
     print("Entering Login info")
     current_url = driver.current_url
-    login_button = driver.find_element_by_class_name("authLinks")
+    login_button = driver.find_element(By.CLASS_NAME, "authLinks")
     login_button.click()
     WebDriverWait(driver, 15).until(EC.url_changes(current_url))
 
     # sign in
     current_url = driver.current_url
-    email_input = driver.find_element_by_name("userLoginId")
-    pw_input = driver.find_element_by_name("password")
+    email_input = driver.find_element(By.NAME, "userLoginId")
+    pw_input = driver.find_element(By.NAME, "password")
     email_input.send_keys(email)
     pw_input.send_keys(pw)
-    sign_in_button = driver.find_element_by_class_name("login-button")
+
+    sign_in_button = driver.find_element(By.CLASS_NAME, "login-button")
     sign_in_button.click()
     WebDriverWait(driver, 15).until(EC.url_changes(current_url))
 
     # choose profile by profile name
     print("Choosing your profile")
-    profiles = driver.find_elements_by_class_name("profile")
+    profiles = driver.find_elements(By.CLASS_NAME, "profile")
     for profile in profiles:
-        name = profile.find_element_by_class_name("profile-name").text
+        name = profile.find_element(By.CLASS_NAME, "profile-name").text
         if name == user_profile:
-            profile_link = profile.find_element_by_class_name("profile-link")
+            profile_link = profile.find_element(By.CLASS_NAME, "profile-link")
             profile_link.click()
             break
     sleep(2)
@@ -72,30 +85,28 @@ def get_my_list():
     # click my list
     print("Finding your list")
     current_url = driver.current_url
-    my_list = driver.find_element_by_link_text("My List")
+    my_list = driver.find_element(By.LINK_TEXT,"My List")
     my_list.click()
     WebDriverWait(driver, 15).until(EC.url_changes(current_url))
 
     # scroll down my list to load all titles
-    logo = driver.find_element_by_class_name("logo")
+    logo = driver.find_element(By.CLASS_NAME,"logo")
     for n in range(1, 10):
         print("Getting list of movies...")
         logo.send_keys(Keys.END)
         sleep(1)
 
     # return list of movies:
-    movies_div = driver.find_elements_by_class_name("title-card")
-    movie_titles = [div.find_element_by_css_selector('a').get_attribute("aria-label") for div in movies_div]
+    movies_div = driver.find_elements(By.CLASS_NAME,"title-card")
+    movie_titles = [div.find_element(By.CSS_SELECTOR, 'a').get_attribute("aria-label") for div in movies_div]
+    driver.quit()
     return movie_titles
 
 def get_expiring_movies():
     url = "https://unogsng.p.rapidapi.com/expiring"
-    # CHANGE COUNTRY TO YOUR ID FROM API
     querystring = {"countrylist":country}
-
     headers = {
         'x-rapidapi-host': "unogsng.p.rapidapi.com",
-        # GET YOUR OWN KEY FOOL
         'x-rapidapi-key': api_key
         }
     response = requests.request("GET", url, headers=headers, params=querystring)
@@ -104,6 +115,7 @@ def get_expiring_movies():
     movie_dicts = data["results"]
     expiring_movies = {movie["title"]:movie["expiredate"] for movie in movie_dicts}
     return expiring_movies
+
 
 my_list = get_my_list()
 expiring_movies = get_expiring_movies()
--- readme.md
+++ readme.md
@@ -1,22 +1,30 @@
-# Netflix My List Expiring Movies
+# Expiring Movies on your Netflix My List
 
 ## Annoyed at Netflix for taking away your movies? 
 
-### Now you don't have to be!
+### This tool checks your My List with movies that are about to expire. 
 
 ------
 ## Installation instructions
 Install selenium
 
-### Change your variables line 16-25
+Choose your webdriver by commenting out the one you do not need. (Pre confiugured for Edge and Chrome drivers).
+
+Create a .env file with:
+
+email= "yournetflixemail@gmail.com"
+pw= "password"
+user_profile= "Kids"
+api_key= "RAPID API KEY HERE" 
+country= "000" 
+
 Get your free UNOGSNG rapid API key here: https://rapidapi.com/unogs/api/unogsng/
 
-Change your country one of the following (if your country is not there, choose one that is geographically closest.)
+List of countries:
+(if your country is not there, choose one that is geographically closest.)
 
 {'Argentina ': 21, 'Australia ': 23, 'Belgium ': 26, 'Brazil ': 29, 'Canada ': 33, 'Switzerland ': 34, 'Germany ': 39, 'France ': 45, 'United Kingdom': 46, 'Mexico ': 65, 'Netherlands ': 67, 'Sweden ': 73, 'United States': 78, 'Iceland ': 265, 'Japan ': 267, 'Portugal ': 268, 'Italy ': 269, 'Spain ': 270, 'Czech Republic ': 307, 'Greece ': 327, 'Hong Kong ': 331, 'Hungary ': 334, 'Israel ': 336, 'India ': 337, 'South Korea': 348, 'Lithuania ': 357, 'Poland ': 392, 'Romania ': 400, 'Russia': 402, 'Singapore ': 408, 'Slovakia ': 412, 'Thailand ': 425, 'Turkey ': 432, 'South Africa': 447}
 
-Finally, enter your Netflix username, password, and profile name (exactly as it appears on Netflix).
-
 # Run main.py. 
 
-###Should work on lists up to 1000 titles. If you have more, that's on you bud! Clean that list up! 
+###Should work on lists up to 1000 titles. If you have more, clean that list up! 
